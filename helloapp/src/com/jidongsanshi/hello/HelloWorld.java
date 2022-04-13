2b16c50cb0fb29b1017f31930aa85103b942f032
jidongsanshi <2218713551@qq.com>
2022-04-13 17:06:18 +0800

    test

---
+++ helloapp/src/com/jidongsanshi/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.jidongsanshi.hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world1");
+    }
+}
---
+++ helloapp/src/com/jidongsanshi/note/Note.java
@@ -0,0 +1,18 @@
+package com.jidongsanshi.note;
+
+/**
+ * 文档注释
+ * 这是一个注释类
+ */
+public class Note {
+    public static void main(String[] args) {
+        //单行注释
+        /*
+        多
+        行
+        注
+        释
+         */
+        System.out.println("这是一个注释类");
+    }
+}
